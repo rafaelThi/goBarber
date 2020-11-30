338e9ac6569362fc5aeae4a78cc3c73c9f5f2091
Rafael de Melo <rafael.pereira20@icloud.com>
2020-11-30 13:35:11 -0300

    Adição de um comentario para melhor entendimento

--- src/Utils/getValidationError.ts
+++ src/Utils/getValidationError.ts
@@ -8,6 +8,7 @@ export default function getValidationError(err: ValidationError): Errors {
   const validatorErrors: Errors = {};
 
   err.inner.forEach(error => {
+    // esse caminho err.inner é possivel ver no console do navegador.
     validatorErrors[error.path] = error.message;
   });
 
