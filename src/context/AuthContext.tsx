777685c81825f51bc3995da2cfa8b7d74c3907f6
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2020-12-02 14:04:47 -0300

    Guardando dados no usuario no Storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,10 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
@@ -16,12 +21,29 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  // essa parte executa no inicio
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarer');
+    const user = localStorage.getItem('@GoBarer');
+
+    if (token && user) {
+      return {
+        token,
+        user: JSON.parse(user),
+      };
+    }
+    return {} as AuthState;
+  });
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', {
       email,
       password,
     });
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarer', token);
+    localStorage.setItem('@GoBarer', JSON.stringify(user));
+    setData({ token, user }); // como o estado ja foi criado, para preencher o token e o user
   }, []);
   return (
     <AuthContext.Provider value={{ name: 'Rafael', signIn }}>
